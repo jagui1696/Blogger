54994bc347767edea0fa2a9c7118d37974e368be
jagui1696 <85315059+jagui1696@users.noreply.github.com>
2021-11-05 13:32:36 -0700

    Update login.php
    nothing

--- login.php
+++ login.php
@@ -30,17 +30,32 @@
                     header('Location: index.php');
                   }
             }
-    
         }
-    
     }
-
 mysqli_close($conn);
 ?>
 
 <!doctype html>
 <html>
-    <head><title>Login Error</title></head>
+    <head>
+        <title>Login Error</title>
+        <style>
+            .container {
+                font-weight: bold;
+                font-family: Arial;
+                display: flex;
+                justify-content: center;
+                text-align: center;
+                align-items: center;
+                background-color: #0C5F89;
+                border: 2px solid #002538;
+                margin: auto;
+                margin-bottom: 10px;
+                width: 250px;
+                padding: 20px;
+            }
+        </style>
+    </head>
     <body style="background-color:grey">
         <form method='post' action="index.php">
             <input type="submit" value="Back to Index" name="submit2">
