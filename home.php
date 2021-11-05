cabe8e025e67e7bac09ed44621a059df473c6694
jagui1696 <85315059+jagui1696@users.noreply.github.com>
2021-11-05 11:08:40 -0700

    Update home.php
    updated initdb button and html

--- home.php
+++ home.php
@@ -12,14 +12,39 @@ if(isset($_POST['submit'])){
     header('Location: index.php');
 }
 
+
+
 ?>
 <!doctype html>
 <html>
-    <head><title>Homepage</title></head>
+    <head><title>COMP440 - Homepage</title>
+    <style>
+    .container {
+        font-weight: bold;
+        font-family: Arial;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        align-items: center;
+        background-color: #0C5F89;
+        border: 2px solid #002538;
+        margin: auto;
+        margin-bottom: 10px;
+        width: 350px;
+        padding: 20px;
+    }
+    </style></head>
     <body style="background-color:grey">
-        <h1>Successfully Logged In</h1>
-        <form method='post' action="">
-            <input type="submit" value="Logout" name="submit">
-        </form>
+        <div id ="header" class="container">
+            <h1>Successfully<br>Logged In</h1>
+        </div>
+        <div id="initdb" class="container">
+            <button type="button" value="initdb">Initialize Database</button></div>
+        </div>
+        <div id="logout" class="container">
+            <form method='post' action="">
+                <input type="submit" value="Logout" name="submit">
+            </form>
+        </div>
     </body>
 </html>
